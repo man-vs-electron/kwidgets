778e205f8094c2d06278834cb046f1ad29a2e268
Man vs Electron <manvselectron@gmail.com>
2022-01-01 11:46:09 -0700

    Fixed a pixelatedgrid redrawing bug

--- src/kwidgets/uix/pixelatedgrid.py
+++ src/kwidgets/uix/pixelatedgrid.py
@@ -23,19 +23,17 @@ class PixelatedGrid(Widget):
 
     def update_canvas(self, *args):
         self.canvas.clear()
-        with self.canvas.before:
-            Color(*self.background)
-            Rectangle(pos = [0,0], size=[self.width, self.height])
         with self.canvas:
+            Color(*self.background)
+            Rectangle(pos = [self.x,self.y], size=[self.width, self.height])
             Color(*self.grid_color)
-            for x in range(0, self.width, self.cell_length):
-                Line(points=[x, 0, x, self.height], width=1)
-            for y in range(self.height, 0, -self.cell_length):
-                Line(points=[0, y, self.width, y], width=1)
-
+            for x in range(0, int(self.width), self.cell_length):
+                Line(points=[self.x + x, self.y, self.x + x, self.y + int(self.height)], width=1)
+            for y in range(int(self.height), 0, -self.cell_length):
+                Line(points=[self.x, self.y + y, self.x + int(self.width), self.y + y], width=1)
             Color(*self.activated_color)
             for x, y in self.activated_cells:
-                Rectangle(pos=[x*self.cell_length, self.height-y*self.cell_length], size=[self.cell_length, self.cell_length])
+                Rectangle(pos=[self.x+x*self.cell_length, self.y+self.height-(y+1)*self.cell_length], size=[self.cell_length, self.cell_length])
 
     def visible_width(self):
         return self.width//self.cell_length
@@ -48,10 +46,35 @@ class PixelatedGridApp(App):
 
     def activate_random(self, *args):
         num_cells=100
-        self.container.activated_cells = set([(random.randint(0, self.container.visible_width()), random.randint(0, self.container.visible_height())) for _ in range(0,num_cells)])
+        visible_width = self.container.ids.thegrid.visible_width()
+        visible_height = self.container.ids.thegrid.visible_height()
+        self.container.ids.thegrid.activated_cells = set([(random.randint(0, visible_width), random.randint(0, visible_height)) for _ in range(0,num_cells)])
 
     def build(self):
-        self.container = PixelatedGrid()
+        self.container = Builder.load_string('''
+BoxLayout:
+    orientation: 'vertical'
+    Button:
+        size_hint: 1, None
+        size: 0, 50
+        text: 'Top Button'
+    BoxLayout:
+        orientation: 'horizontal'
+        Button:
+            size_hint: None, 1
+            size: 50, 0
+            text: 'L'
+        PixelatedGrid:
+            id: thegrid
+        Button:
+            size_hint: None, 1
+            size: 50, 0
+            text: 'R'
+    Button:
+        size_hint: 1, None
+        size: 0, 50
+        text: 'Bottom Button'
+''')
         Clock.schedule_interval(self.activate_random, .5)
         return self.container
 
